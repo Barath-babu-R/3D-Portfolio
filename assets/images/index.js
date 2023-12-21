3148618b27c6ec33fb00392a45b05d38f6ee5130
Barath <147180302+Barath-babu-R@users.noreply.github.com>
2023-12-21 18:39:45 +0530

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import tn from './tn.png'
+import dn from './dn.png'
+import ibm from './ibm.png'
+import sih from './sih.png'
+import logo from './logo.svg'
+
+export {
+    hero,  
+    tn,
+    dn,
+    ibm,
+    sih,
+    logo
+}
---
+++ constants/index.js
@@ -0,0 +1,217 @@
+import {dn, ibm, sih, tn} from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "NAAN MUDHALVAN ACHEIVER",
+        company_name: "Honourable Tamilnadu CM",
+        icon: tn ,
+        iconBg: "#accbe1",
+        date: "May - 2023",
+        points: [
+"Received the Naan Mudhalvan Achiever Award from Chief Minister M.K. Stalin for providing outstanding project ideas in the Naan Mudhalvan scheme event.",
+"Recognized for exceptional creativity and innovation in proposing projects aligned with the Naan Mudhalvan scheme's goals.",
+"Demonstrated leadership by presenting visionary project ideas that contributed to the scheme's success.",
+"Made a significant impact with project ideas aimed at addressing real-world challenges and promoting Tamil Nadu's development through the Naan Mudhalvan initiative.",
+        ],
+    },
+    {
+        title: "INSTAMIND",
+        company_name: "Dunzo",
+        icon: dn,
+        iconBg: "#fbc3bc",
+        date: "Oct - 2022",
+        points: [ 
+"Won first place in the INSTAMIND competition by providing the winning solution to a problem presented by DUNZO.",
+"Demonstrated strong problem-solving abilities by impressing judges and competitors with a practical solution.",
+"Received recognition at the event held at Jeppiaar Engineering College for exceptional performance.",
+"Applied skills effectively to address a challenge posed by Dunzo, showcasing practical problem-solving capabilities. ",
+          
+    ],
+    },
+    {
+        title: "IBM",
+        company_name: "Swms",
+        icon: ibm,
+        iconBg: "#b7e4c7",
+        date: "Jul 2022 - Dec 2023",
+        points: [
+"Successfully designed and implemented the SMART WASTE MANAGEMENT SYSTEM within the IoT domain during my tenure at IBM.",
+"Demonstrated expertise in IoT technologies by developing a system that addresses real-world challenges in waste management.",
+"Recognized as a NALAYATHIRAN Project indicating its significance within IBM and possibly signifying its impact or innovation in the company's projects.",
+"Applied innovative solutions to create a smart system that streamlines waste management processes, showcasing problem-solving skills and innovative thinking.",
+        ],
+    },
+    {
+        title: "SMART INDIA HACKATHON",
+        company_name: "SIH",
+        icon: sih,
+        iconBg: "#a2d2ff",
+        date: "Aug - 2022",
+        points: [
+"Won the Best Performer Award at the National Level Smart India Hackathon for developing a website providing information on government schemes.",
+"Demonstrated innovation by creating a website that effectively communicates government scheme details to the public.",
+"Received national-level recognition for the website's design and functionality, showcasing its impact and relevance.",
+"Achieved success in a competitive environment, highlighting the ability to deliver impactful solutions.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Barath-babu-R',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/barath-r-18b2581b1',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: '3D Vistas Website',
+        description: 'A 3D website made with Three.js offers immersive, interactive 3D graphics on web browser. It enables engaging scenes, animations, and effects for unique web experiences.',
+        link: 'https://github.com/adrianhajdin/social_media_app',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-red',
+        name: 'Food Delivery Website',
+        description: 'Developed a food delivery website lets users order from restaurants, browse menus, place orders, and have food delivered, offering a convenient dining experience.',
+        link: 'https://github.com/Barath-babu-R/Food-Delivery-Website',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Library Management Website',
+        description: 'A library book management website helps libraries organize their collections and enables users to search for books, check availability, and borrow. It also helps librarians to manage inventory.',
+        link: 'https://github.com/Barath-babu-R/Library-Management-Website',
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-blue',
+        name: 'Movie Recommendation Website',
+        description: 'Designed a movie recommendation website suggests films based on user preferences. It offers movie details, ratings, and reviews to help users discover new films they might enjoy watching.',
+        link: 'https://github.com/Barath-babu-R/Movie-Recommendation-website',
+    },
+   
+    
+];
